b8817eb0d542f1fe3f79d71e581e4c784b5c15e4
doomcatlee <alex.kun.lee@gmail.com>
2024-03-13 12:42:55 -0700

    [Feat] Added GET and POST endpoints

---
+++ src/test/java/com/example/cashcard/CashCardJsonTest.java
@@ -0,0 +1,34 @@
+package com.example.cashcard;
+
+import com.example.cashcard.model.CashCard;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+public class CashCardJsonTest {
+
+    @Test
+    void firstTest() {
+        assertThat(42).isEqualTo(42);
+    }
+
+    @Autowired
+    private JacksonTester<CashCard> json;
+
+    @Test
+    void cashCardSerializationTest() throws IOException {
+        CashCard cashCard = new CashCard(99L, 123.45);
+
+        assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
+        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
+        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
+        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
+        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
+    }
+}
